fc21c1090c9080f752098e3ba6a74a487dd8f4ae
xk97 <xhkong@mail.utexas.edu>
2019-08-10 12:51:06 -0500

    finished monte_hall

--- monty_hall.py
+++ monty_hall.py
@@ -1,7 +1,9 @@
 #%% Monty Hall problem
+# n_doors with 1 door with award, pick inital y, k rounds of opportunity to switch
 import numpy as np
 
 n_trails, n_doors = 1000, 3
+k = n_doors - 2
 
 idx_X = np.random.choice(range(n_doors), n_trails)
 X = np.zeros((n_trails, n_doors))
@@ -12,4 +14,9 @@ print(X)
 y = np.random.choice(range(n_doors), n_trails)
 print(f'corrected ratio: {np.sum(y==idx_X) / n_trails}')
 #%% change choice after k round
+for i in range(k):  # K<= X.shape[1] - 2
+    # random drop idx not in (y, indx_X)
+    pass
+print(f'win by switching: {np.sum(y != idx_X) / n_trails}')
+
 
